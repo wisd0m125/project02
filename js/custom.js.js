281598d9fd499c3f37157682e01e3a5ec5a2af63
wisd0m125 <jsjs970125@gmail.com>
2022-08-16 17:53:07 +0900

    슬라이드 수정

---
+++ js/custom.js.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".mySwiper01", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next btn01",
+      prevEl: ".swiper-button-prev btn01",
+    },
+  });
+  
+  var swiper = new Swiper(".mySwiper02", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  
