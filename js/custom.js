7c262f93d84e4c8159373dd097bd03270218f04c
wisd0m125 <jsjs970125@gmail.com>
2022-10-21 10:57:11 +0900

    반응형 구현

--- js/custom.js
+++ js/custom.js
@@ -1,58 +1,77 @@
 var swiper = new Swiper(".mySwiper01", {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    slidesPerGroup: 4,
-    loop: true,
-    autoplay: {
-      delay:6000,
-      disableOnInteraction: false,
+  slidesPerView: 2,
+  spaceBetween: 10,
+  slidesPerGroup: 1,
+  loop: true,
+  speed: 2000,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    501: {
+      slidesPerView: 3,
+      spaceBetween: 20,
     },
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    765: {
+      slidesPerView: 4,
+      spaceBetween: 10,
     },
-    navigation: {
-      nextEl: ".swiper-button-next btn01",
-      prevEl: ".swiper-button-prev btn01",
-    },
-  });
-  var swiper1=new Swiper(".swiper mySwiper01",{
-    loop:true,
-    autoplay:5500,
-    autoplayDisableOnInteraction:false,
-    pagination:{
-      el:".swiper-pagination",
-      Clickable:true,
-    }
-  })
-  var swiper = new Swiper(".mySwiper02", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    autoplay: {
-      delay:6000,
-      disableOnInteraction: false,
-    },
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  var swiper1=new Swiper(".swiper mySwiper02",{
-    loop:true,
-    autoplay:5500,
-    autoplayDisableOnInteraction:false,
-    pagination:{
-      el:".swiper-pagination",
-      Clickable:true,
-    }
-  })
+  },
+});
 
-  
+var swiper = new Swiper(".mySwiper02", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  slidesPerGroup: 1,
+  loop: true,
+  speed: 2000,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    501: {
+      slidesPerView: 2,
+      spaceBetween: 5,
+    },
+    765: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+    },
+  },
+});
+var swiper1 = new Swiper(".swiper mySwiper02", {
+  loop: true,
+  autoplay: 5500,
+  autoplayDisableOnInteraction: false,
+  pagination: {
+    el: ".swiper-pagination",
+    Clickable: true,
+  },
+});
+
+let $slides = document.querySelectorAll(".swiper-slide");
+for (let i of $slides) {
+  i.addEventListener("mouseover", function () {
+    swiper.autoplay.stop();
+  });
+  i.addEventListener("mouseout", function () {
+    swiper.autoplay.start();
+  });
+}
--- js/topbtn.js
+++ js/topbtn.js
@@ -1,17 +1,14 @@
-const btt=document.querySelector('#back_to_top');
-let scrollAmount;
+let Top = document.querySelector(".back_to_top");
 
-window.addEventListener('scroll',function(){
-    scrollAmount=this.scrollY;
-    console.log(scrollAmount)
-    if(scrollAmount>scrollAmount/4){
-        btt.classList.add('visible');
-    }else{
-        btt.classList.remove('visible')  
-    }
-})
-btt.addEventListener('click',function(e){
-    e.preventDefault();
-    document.querySelector(btt.getAttribute('href')).
-    scrollIntoView({behavior:'smooth'})
-})
+window.addEventListener("scroll", function () {
+  if (this.scrollY > 200) {
+    Top.classList.add("on");
+  } else {
+    Top.classList.remove("on");
+  }
+});
+
+Top.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
